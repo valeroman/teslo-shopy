720a9f5cb4c6cafd06084c519991597f17251389
valeroman <valeroman@gmail.com>
2024-01-08 06:51:30 -0500

    Connfiguracionn zustand

--- src/components/ui/sidebar/Sidebar.tsx
+++ src/components/ui/sidebar/Sidebar.tsx
@@ -1,133 +1,154 @@
 'use client';
 
-import Link from 'next/link';
 import React from 'react'
-import { IoCloseOutline, IoLogInOutline, IoLogOutOutline, IoPeopleOutline, IoPersonOutline, IoSearchOutline, IoShirtOutline, IoTicketOutline } from 'react-icons/io5'
+import Link from 'next/link';
+import clsx from 'clsx';
+
+import { IoCloseOutline, IoLogInOutline, IoLogOutOutline, IoPeopleOutline, IoPersonOutline, IoSearchOutline, IoShirtOutline, IoTicketOutline } from 'react-icons/io5';
+import { useUIStore } from '@/store';
 
 export const Sidebar = () => {
-  return (
-    <div>
 
-        {/* Background Blac */}
-        <div 
-            className='fixed top-0 left-0 w-screen h-screen z-10 bg-black opacity-30'
-        />
+    const isSideMenuOpen = useUIStore( state => state.isSideMenuOpen );
+    const closeMenu = useUIStore( state => state.closeSideMenu );
+    return (
+        <div>
 
-        {/* Blur */}
-        <div 
-            className=' fade-in fixed top-0 left-0 w-screen h-screen z-10 backdrop-filter backdrop-blur-sm'
-        />
+            {/* Background Blac */}
+            {
+                isSideMenuOpen && (
+                    <div
+                        className='fixed top-0 left-0 w-screen h-screen z-10 bg-black opacity-30'
+                    />
+                )
+            }
 
-        {/* Sidemenu */}
-        <nav 
-            // todo: efecto slide
-            className='fixed p-5 right-0 top-0 w-[500px] h-screen bg-white z-20 shadow-2xl transform transition-all duration-300'
-        >
-            <IoCloseOutline 
-                size={ 50 }
-                className="absolute top-5 right-5 cursor-pointer"
-                onClick={ () => console.log('Click')}
-            />
+            {/* Blur */}
+            {
+                isSideMenuOpen && (
+                    <div
+                        className=' fade-in fixed top-0 left-0 w-screen h-screen z-10 backdrop-filter backdrop-blur-sm'
+                        onClick={ closeMenu }
+                    />
+                )
+            }
 
-
-            {/* Input */}
-            <div className='relative mt-14'>
-                <IoSearchOutline 
-                    size={ 20 }
-                    className='absolute top-2 left-2'
-                />
-
-                <input
-                    type='text'
-                    placeholder='Buscar' 
-                    className='w-full bg-gray-50 rounded pl-10 py-1 pr-10 border-b-2 text-xl border-gray-200 focus:outline-none focus:border-blue-500'
-                />
-            </div>
-
-            {/* Opciones Menu */}
-            <Link
-                href="/"
-                className='flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all'
+            {/* Sidemenu */}
+            <nav
+                className={
+                    clsx(
+                        'fixed p-5 right-0 top-0 w-[500px] h-screen bg-white z-20 shadow-2xl transform transition-all duration-300',
+                        {
+                            'translate-x-full': !isSideMenuOpen
+                        }
+                    )
+                }
             >
-                <IoPersonOutline 
-                    size={ 30 }
+                <IoCloseOutline
+                    size={50}
+                    className="absolute top-5 right-5 cursor-pointer"
+                    onClick={ closeMenu }
                 />
-                <span className='ml-3 text-xl'>Perfil</span>
-            </Link>
 
-            <Link
-                href="/"
-                className='flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all'
-            >
-                <IoTicketOutline 
-                    size={ 30 }
-                />
-                <span className='ml-3 text-xl'>Ordenes</span>
-            </Link>
 
-            <Link
-                href="/"
-                className='flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all'
-            >
-                <IoLogInOutline 
-                    size={ 30 }
-                />
-                <span className='ml-3 text-xl'>Ingresar</span>
-            </Link>
+                {/* Input */}
+                <div className='relative mt-14'>
+                    <IoSearchOutline
+                        size={20}
+                        className='absolute top-2 left-2'
+                    />
 
-            <Link
-                href="/"
-                className='flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all'
-            >
-                <IoLogOutOutline 
-                    size={ 30 }
-                />
-                <span className='ml-3 text-xl'>Salir</span>
-            </Link>
+                    <input
+                        type='text'
+                        placeholder='Buscar'
+                        className='w-full bg-gray-50 rounded pl-10 py-1 pr-10 border-b-2 text-xl border-gray-200 focus:outline-none focus:border-blue-500'
+                    />
+                </div>
 
-            {/* Line Separator */}
-            <div className='w-full h-px bg-gray-200 my-10'/>
+                {/* Opciones Menu */}
+                <Link
+                    href="/"
+                    className='flex items-center mt-5 p-2 hover:bg-gray-100 rounded transition-all'
+                >
+                    <IoPersonOutline
+                        size={30}
+                    />
+                    <span className='ml-3 text-xl'>Perfil</span>
+                </Link>
 
-            <Link
-                href="/"
-                className='flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all'
-            >
-                <IoShirtOutline 
-                    size={ 30 }
-                />
-                <span className='ml-3 text-xl'>Productos</span>
-            </Link>
+                <Link
+                    href="/"
+                    className='flex items-center mt-5 p-2 hover:bg-gray-100 rounded transition-all'
+                >
+                    <IoTicketOutline
+                        size={30}
+                    />
+                    <span className='ml-3 text-xl'>Ordenes</span>
+                </Link>
 
-            <Link
-                href="/"
-                className='flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all'
-            >
-                <IoTicketOutline 
-                    size={ 30 }
-                />
-                <span className='ml-3 text-xl'>Ordenes</span>
-            </Link>
+                <Link
+                    href="/"
+                    className='flex items-center mt-5 p-2 hover:bg-gray-100 rounded transition-all'
+                >
+                    <IoLogInOutline
+                        size={30}
+                    />
+                    <span className='ml-3 text-xl'>Ingresar</span>
+                </Link>
 
-            <Link
-                href="/"
-                className='flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all'
-            >
-                <IoTicketOutline 
-                    size={ 30 }
-                />
-                <span className='ml-3 text-xl'>Ordenes</span>
-            </Link><Link
-                href="/"
-                className='flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all'
-            >
-                <IoPeopleOutline 
-                    size={ 30 }
-                />
-                <span className='ml-3 text-xl'>Usuarios</span>
-            </Link>
+                <Link
+                    href="/"
+                    className='flex items-center mt-5 p-2 hover:bg-gray-100 rounded transition-all'
+                >
+                    <IoLogOutOutline
+                        size={30}
+                    />
+                    <span className='ml-3 text-xl'>Salir</span>
+                </Link>
 
-        </nav>
+                {/* Line Separator */}
+                <div className='w-full h-px bg-gray-200 my-10' />
 
-    </div>
-  )
+                <Link
+                    href="/"
+                    className='flex items-center mt-5 p-2 hover:bg-gray-100 rounded transition-all'
+                >
+                    <IoShirtOutline
+                        size={30}
+                    />
+                    <span className='ml-3 text-xl'>Productos</span>
+                </Link>
+
+                <Link
+                    href="/"
+                    className='flex items-center mt-5 p-2 hover:bg-gray-100 rounded transition-all'
+                >
+                    <IoTicketOutline
+                        size={30}
+                    />
+                    <span className='ml-3 text-xl'>Ordenes</span>
+                </Link>
+
+                <Link
+                    href="/"
+                    className='flex items-center mt-5 p-2 hover:bg-gray-100 rounded transition-all'
+                >
+                    <IoTicketOutline
+                        size={30}
+                    />
+                    <span className='ml-3 text-xl'>Ordenes</span>
+                </Link><Link
+                    href="/"
+                    className='flex items-center mt-5 p-2 hover:bg-gray-100 rounded transition-all'
+                >
+                    <IoPeopleOutline
+                        size={30}
+                    />
+                    <span className='ml-3 text-xl'>Usuarios</span>
+                </Link>
+
+            </nav>
+
+        </div>
+    )
 }
--- src/components/ui/top-menu/TopMenu.tsx
+++ src/components/ui/top-menu/TopMenu.tsx
@@ -1,49 +1,56 @@
+'use client';
+
 import Link from 'next/link';
 import { IoSearchOutline, IoCartOutline } from 'react-icons/io5';
 import { titleFont } from '@/config/fonts'
+import { useUIStore } from '@/store';
 
 
 export const TopMenu = () => {
-  return (
-    <nav className='flex px-5 justify-between items-center w-full'>
 
-        {/* Logo */}
-        <div>
-            <Link
-                href="/"
-            >
-                <span className={`${ titleFont.className } antialiased font-bold`}>Teslo</span>
-                <span> | Shop</span>
-            </Link>
-        </div>
+    const openMenu = useUIStore( state => state.openSideMenu );
 
-        {/* Center Menu */}
-        <div className='hidden sm:block'>
-            <Link className='m-2 p-2 rounded-md transition-all hover:bg-gray-100' href="/category/men">Hombres</Link>
-            <Link className='m-2 p-2 rounded-md transition-all hover:bg-gray-100' href="/category/women">Mujeres</Link>
-            <Link className='m-2 p-2 rounded-md transition-all hover:bg-gray-100' href="/category/kids">Niños</Link>
-        </div>
+    return (
+        <nav className='flex px-5 justify-between items-center w-full'>
 
-        {/* Search, Cacrt, Menu */}
-        <div className='flex items-center'>
-            <Link href="/search" className='mx-2'>
-                <IoSearchOutline className="w-5 h-5" />
-            </Link>
+            {/* Logo */}
+            <div>
+                <Link
+                    href="/"
+                >
+                    <span className={`${titleFont.className} antialiased font-bold`}>Teslo</span>
+                    <span> | Shop</span>
+                </Link>
+            </div>
 
-            <Link href="/cart" className='mx-2'>
-                <div className='relative'>
-                    <span className='absolute text-xs rounded-full px-1 font-bold -top-2 -right-2 bg-blue-700 text-white'>3</span>
-                    <IoCartOutline className="w-5 h-5" />
-                </div>
-            </Link>
+            {/* Center Menu */}
+            <div className='hidden sm:block'>
+                <Link className='m-2 p-2 rounded-md transition-all hover:bg-gray-100' href="/category/men">Hombres</Link>
+                <Link className='m-2 p-2 rounded-md transition-all hover:bg-gray-100' href="/category/women">Mujeres</Link>
+                <Link className='m-2 p-2 rounded-md transition-all hover:bg-gray-100' href="/category/kids">Niños</Link>
+            </div>
 
-            <button
-                className='m-2 p-2 rounded-md transition-all hover:bg-gray-100'
-            >
-                Menu
-            </button>
+            {/* Search, Cacrt, Menu */}
+            <div className='flex items-center'>
+                <Link href="/search" className='mx-2'>
+                    <IoSearchOutline className="w-5 h-5" />
+                </Link>
 
-        </div>
-    </nav>
-  )
+                <Link href="/cart" className='mx-2'>
+                    <div className='relative'>
+                        <span className='absolute text-xs rounded-full px-1 font-bold -top-2 -right-2 bg-blue-700 text-white'>3</span>
+                        <IoCartOutline className="w-5 h-5" />
+                    </div>
+                </Link>
+
+                <button
+                    className='m-2 p-2 rounded-md transition-all hover:bg-gray-100'
+                    onClick={ openMenu }
+                >
+                    Menu
+                </button>
+
+            </div>
+        </nav>
+    )
 }
