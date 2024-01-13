5629ccf15b6c4521d1f6bb991540410f885059cc
valeroman <valeroman@gmail.com>
2024-01-13 10:17:52 -0500

    footer, login, register y mejors cart

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -18,11 +18,11 @@ export default function () {
 
   return (
     <div className="flex justify-center items-center mb-72 px-10 sm:px-0">
-      <div className="flex flex-col w-[1000px] bg-yellow-500">
+      <div className="flex flex-col w-[1000px]">
 
         <Title title="Carrito" />
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-10 bg-green-300">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
 
           {/* Carrito */}
           <div className="flex flex-col mt-5">
@@ -57,7 +57,7 @@ export default function () {
           </div>
 
           {/* Checkout - Resumen de orden */}
-          <div className="bg-white rounded-xl shadow-xl p-7">
+          <div className="bg-white rounded-xl shadow-xl p-7 h-fit">
             <h2 className="text-2xl mb-2">Resumen de orden</h2>
 
             <div className="grid grid-cols-2">
--- src/components/index.ts
+++ src/components/index.ts
@@ -14,3 +14,5 @@ export * from './product/quantity-selector/QuantitySelector';
 
 export * from './product/slideshow/ProductSlideshow';
 export * from './product/slideshow/ProductMobileSlideshow';
+
+export * from './ui/footer/Footer';
