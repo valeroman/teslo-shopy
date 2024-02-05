a197c637787d913ff5358d433907acc3893c3213
valeroman <valeroman@gmail.com>
2024-02-05 14:34:40 -0500

    Paginacion Funcionamiento generar mostrar numero de pagina

--- src/components/ui/pgination/Pagination.tsx
+++ src/components/ui/pgination/Pagination.tsx
@@ -1,40 +1,87 @@
-import { IoChevronBack, IoChevronBackOutline, IoChevronForwardOutline } from "react-icons/io5";
+'use client';
+
+import clsx from "clsx";
+import Link from "next/link";
+import { redirect, usePathname, useSearchParams } from "next/navigation";
+import { IoChevronBackOutline, IoChevronForwardOutline } from "react-icons/io5";
+
+import { generatePaginationNumbers } from '../../../utils/generatePaginationNumbers';
 
 interface Props {
     totalPages: number;
 }
 
 export const Pagination = ({ totalPages }: Props) => {
+
+    const pathname = usePathname();
+    const searchParams = useSearchParams();
+
+    const pageString = searchParams.get('page') ?? 1;
+    const currentPage = isNaN( +pageString ) ? 1 : +pageString;
+
+    if ( currentPage < 1 || isNaN(+pageString) ) {
+        redirect( pathname );
+    }
+    
+    const allPages = generatePaginationNumbers( currentPage, totalPages );
+
+    const createPageUrl = ( pageNumber: number | string ) => {
+        // Construir parametros del url
+        const params = new URLSearchParams( searchParams );
+
+        if ( pageNumber === '...' ) {
+            return `${ pathname }?${ params.toString() }`
+        }
+
+        if ( +pageNumber <= 0 ) {
+            return `${ pathname }`; // href="/kid"
+        }
+
+        if ( +pageNumber > totalPages ) {                 // Quedate en la misma pagina
+            return `${ pathname }?${ params.toString() }` // Next >
+        }
+
+        params.set('page', pageNumber.toString());
+        return `${ pathname }?${ params.toString() }`
+    }
+
     return (
         <div className="flex  text-center justify-center mt-10 mb-32">
             <nav aria-label="Page navigation example">
                 <ul className="flex list-style-none">
                     <li className="page-item">
-                        <a
+                        <Link
                             className="page-link relative block py-1.5 px-3 border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none"
-                            href="#">
+                            href={ createPageUrl( currentPage - 1 ) }>
                             <IoChevronBackOutline size={30} />
-                        </a>
+                        </Link>
                     </li>
 
-                    <li className="page-item"><a
-                        className="page-link relative block py-1.5 px-3 border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none"
-                        href="#">1</a></li>
+                    { allPages.map((page) => (
 
-                    <li className="page-item active"><a
-                        className="page-link relative block py-1.5 px-3 border-0 bg-blue-600 outline-none transition-all duration-300 rounded text-white hover:text-white hover:bg-blue-600 shadow-md focus:shadow-md"
-                        href="#">2 <span className="visually-hidden"></span></a></li>
+                        <li key={ page } className="page-item">
+                            <Link
+                                className={
+                                    clsx(
+                                        "page-link relative block py-1.5 px-3 border-0 outline-none transition-all duration-300 rounded text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none",
+                                        {
+                                            'bg-blue-600 shadow-sm text-white hover:text-white hover:bg-blue-700': page === currentPage
+                                        }
+                                    )
+                                }
+                                href={ createPageUrl( page )}>
+                                    { page }
+                            </Link>
+                        </li>
+                    ))}
 
-                    <li className="page-item"><a
-                        className="page-link relative block py-1.5 px-3 border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none"
-                        href="#">3</a></li>
 
                     <li className="page-item">
-                        <a
+                        <Link
                             className="page-link relative block py-1.5 px-3 border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none"
-                            href="#">
+                            href={ createPageUrl( currentPage + 1 ) }>
                             <IoChevronForwardOutline size={30} />
-                        </a>
+                        </Link>
                     </li>
                 </ul>
             </nav>
