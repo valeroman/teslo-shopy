7a68f89404cb817893ceaf1d72eb5286a5ed9795
valeroman <valeroman@gmail.com>
2024-03-16 08:07:14 -0500

    Optimizar metadata

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,5 +1,6 @@
 export const revalidate = 604800; // 7 dias
 
+import { Metadata, ResolvingMetadata } from "next";
 import { getProductBySlug } from "@/actions";
 import { ProductMobileSlideshow, ProductSlideshow, QuantitySelector, SizeSelector } from "@/components";
 import { titleFont } from "@/config/fonts";
@@ -10,6 +11,30 @@ import { StockLabel } from '../../../../components/product/stock-label/StockLabe
 interface Props {
   params: {
     slug: string;
+  }
+}
+
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  // read route params
+  const slug = params.slug
+ 
+  // fetch data
+  const product = await getProductBySlug(slug);
+ 
+  // optionally access and extend (rather than replace) parent metadata
+  //const previousImages = (await parent).openGraph?.images || []
+ 
+  return {
+    title: product?.title ?? "Producto no encontrado",
+    description: product?.description ?? "",
+    openGraph: {
+      title: product?.title ?? "Producto no encontrado",
+      description: product?.description ?? "",
+      images: [`/products/${ product?.images[1]}`],
+    },
   }
 }
 
